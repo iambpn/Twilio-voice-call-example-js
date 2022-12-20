2f9369e2631557594370dc140f79a7e2438ca67a
Bipin Maharjan <bopon.maharjan@gmail.com>
2022-12-20 15:38:34 +0545

    adding server sent event

--- index.js
+++ index.js
@@ -10,6 +10,13 @@ const app = express();
 const VoiceGrant = AccessToken.VoiceGrant;
 const { config } = require("./.config.js");
 
+app.use(
+  express.urlencoded({
+    extended: false,
+  })
+);
+app.use(express.json());
+
 app.use("/static", express.static(path.join(__dirname, "static")));
 app.use(cors());
 
@@ -75,13 +82,118 @@ app.get("/handle-calls", twilio.webhook({ validate: false }), (req, res) => {
   res.send(twiml.toString());
 });
 
+/**
+ * key is to_number and value is string[]
+ * @type {Object.<string,string[]>}
+ * @constant
+ * */
+const eventsByNumber = {};
+
+/** @typedef {object} CallbackEvent
+ * @property {string} Called
+ * @property {string} ParentCallSid
+ * @property {string} ToState
+ * @property {string} CallerCountry
+ * @property {string} Direction
+ * @property {string} Timestamp
+ * @property {string} CallbackSource
+ * @property {string} CallerState
+ * @property {string} ToZip
+ * @property {string} SequenceNumber
+ * @property {string} CallSid
+ * @property {string} To
+ * @property {string} CallerZip
+ * @property {string} ToCountry
+ * @property {string} CalledZip
+ * @property {string} ApiVersion
+ * @property {string} CalledCity
+ * @property {string} CallStatus
+ * @property {string} From
+ * @property {string} AccountSid
+ * @property {string} CalledCountry
+ * @property {string} CallerCity
+ * @property {string} ToCity
+ * @property {string} FromCountry
+ * @property {string} Caller
+ * @property {string} FromCity
+ * @property {string} CalledState
+ * @property {string} FromZip
+ * @property {string} FromState
+ */
+
 app.get("/callback-status", (req, res) => {
   console.log("\nCallback-status");
   console.log("Body:", JSON.stringify(req.body));
   console.log("Params:", JSON.stringify(req.params));
   console.log("Query:", JSON.stringify(req.query));
 
+  /**
+   * @type {CallbackEvent}
+   */
+  const query = req.query;
+  const key = `${query.To.slice(1)}_${query.From.slice(1)}`;
+
+  if (eventsByNumber[key]) {
+    eventsByNumber[key].push(query.CallStatus);
+  } else {
+    console.log("\n eventByNumber not found", Object.keys(eventsByNumber));
+  }
+
+  if (responses[key]) {
+    console.log("\n response sent:", eventsByNumber[key]);
+    responses[key].write(`data: ${JSON.stringify(eventsByNumber[key])} \n\n`);
+  } else {
+    console.log("\n Response not found", Object.keys(responses));
+  }
+
   return res.send("<response></response>");
+});
+
+/**
+ * @type {Object.<string>}
+ */
+const responses = {};
+app.get("/getEvents", (req, res) => {
+  res.setHeader("Content-Type", "text/event-stream");
+  res.setHeader("Connection", "keep-alive");
+  res.flushHeaders(); // ack
+
+  /**
+   * @type {{
+   *  to: string,
+   *  from: string
+   * }}
+   */
+  const query = req.query;
+  if (!query.to || !query.from) {
+    res.write("event: closed\n");
+    res.write("data: req query error\n\n");
+    return;
+  }
+
+  const key = `${query.to.slice(1)}_${query.from.slice(1)}`;
+  res.on("close", () => {
+    console.log("response closed by client");
+    res.end();
+
+    // clean up
+    console.log(key);
+    delete responses[key];
+    delete eventsByNumber[key];
+  });
+
+  if (eventsByNumber[key]) {
+    eventsByNumber[key] = [];
+    console.log("\n response sent:", eventsByNumber[key]);
+    res.write("event: closed\n");
+    res.write(`data: ${JSON.stringify(eventsByNumber[key])} \n\n`);
+    return;
+  } else {
+    eventsByNumber[key] = [];
+    console.log("\n Key not found in eventsByNumber.", key);
+  }
+
+  responses[key] = res;
 });
 
 app.listen(3000, () => {
--- static/js/main.js
+++ static/js/main.js
@@ -1,115 +1,144 @@
 $(function () {
-    var device;
+  var device;
+  var sse;
 
-    log("Requesting Access Token...");
-    // Using a relative link to access the Voice Token function
-    // Making api call
-    fetch("./token")
-        .then(function(data){
-            return data.json()
-        })
-        .then(function (data) {
-            log("Got a token.");
-            console.log("Token: " + data.token);
+  log("Requesting Access Token...");
+  // Using a relative link to access the Voice Token function
+  // Making api call
+  fetch("./token")
+    .then(function (data) {
+      return data.json();
+    })
+    .then(function (data) {
+      log("Got a token.");
+      console.log("Token: " + data.token);
 
-            // Setup Twilio.Device
-            device = new Twilio.Device(data.token, {
-                // Set Opus as our preferred codec. Opus generally performs better, requiring less bandwidth and
-                // providing better audio quality in restrained network conditions. Opus will be default in 2.0.
-                codecPreferences: ["opus", "pcmu"],
-                // Use fake DTMF tones client-side. Real tones are still sent to the other end of the call,
-                // but the client-side DTMF tones are fake. This prevents the local mic capturing the DTMF tone
-                // a second time and sending the tone twice. This will be default in 2.0.
-                fakeLocalDTMF: true,
-                // Use `enableRingingState` to enable the device to emit the `ringing`
-                // state. The TwiML backend also needs to have the attribute
-                // `answerOnBridge` also set to true in the `Dial` verb. This option
-                // changes the behavior of the SDK to consider a call `ringing` starting
-                // from the connection to the TwiML backend to when the recipient of
-                // the `Dial` verb answers.
-                enableRingingState: true,
-                debug: true,
-            });
+      // Setup Twilio.Device
+      device = new Twilio.Device(data.token, {
+        // Set Opus as our preferred codec. Opus generally performs better, requiring less bandwidth and
+        // providing better audio quality in restrained network conditions. Opus will be default in 2.0.
+        codecPreferences: ["opus", "pcmu"],
+        // Use fake DTMF tones client-side. Real tones are still sent to the other end of the call,
+        // but the client-side DTMF tones are fake. This prevents the local mic capturing the DTMF tone
+        // a second time and sending the tone twice. This will be default in 2.0.
+        fakeLocalDTMF: true,
+        // Use `enableRingingState` to enable the device to emit the `ringing`
+        // state. The TwiML backend also needs to have the attribute
+        // `answerOnBridge` also set to true in the `Dial` verb. This option
+        // changes the behavior of the SDK to consider a call `ringing` starting
+        // from the connection to the TwiML backend to when the recipient of
+        // the `Dial` verb answers.
+        enableRingingState: true,
+        debug: true,
+      });
 
-            device.on("ready", function (device) {
-                log("Twilio.Device Ready!");
-            });
+      device.on("ready", function (device) {
+        log("Twilio.Device Ready!");
+      });
 
-            device.on("error", function (error) {
-                log("Twilio.Device Error: " + error.message);
-            });
+      device.on("error", function (error) {
+        log("Twilio.Device Error: " + error.message);
+      });
 
-            device.on("connect", function (conn) {
-                log("Successfully established call ! ");
-                $("#modal-call-in-progress").modal("show");
-            });
+      device.on("connect", function (conn) {
+        log("Successfully established call ! ");
+        $("#modal-call-in-progress").modal("show");
+      });
 
-            device.on("disconnect", function (conn) {
-                log("Call ended.");
-                $(".modal").modal("hide");
-            });
+      device.on("disconnect", function (conn) {
+        log("Call ended.");
+        sse.close();
+        $(".modal").modal("hide");
+      });
 
-            device.on("incoming", function (conn) {
-                console.log(conn.parameters);
-                log("Incoming connection from " + conn.parameters.From);
-                $("#callerNumber").text(conn.parameters.From);
-                $("#txtPhoneNumber").text(conn.parameters.From);
+      device.on("incoming", function (conn) {
+        console.log(conn.parameters);
+        log("Incoming connection from " + conn.parameters.From);
+        $("#callerNumber").text(conn.parameters.From);
+        $("#txtPhoneNumber").text(conn.parameters.From);
 
-                $("#modal-incomming-call").modal("show");
+        $("#modal-incomming-call").modal("show");
 
-                $(".btnReject").bind("click", function () {
-                    $(".modal").modal("hide");
-                    log("Rejected call ...");
-                    conn.reject();
-                });
-
-                $(".btnAcceptCall").bind("click", function () {
-                    $(".modal").modal("hide");
-                    log("Accepted call ...");
-                    conn.accept();
-                });
-            });
-        })
-        .catch(function (err) {
-            console.log(err);
-            log("Could not get a token from server!");
+        $(".btnReject").bind("click", function () {
+          $(".modal").modal("hide");
+          log("Rejected call ...");
+          conn.reject();
         });
 
-    // Bind button to make call
-    $("#btnDial").bind("click", async function () {
-        $("#modal-dial").modal("hide");
-
-        // get the phone number to connect the call to
-        var params = {
-            To: document.getElementById("phoneNumber").value,
-        };
-
-        // output destination number
-        $("#txtPhoneNumber").text(params.To);
-
-        console.log("Calling " + params.To + "...");
-        if (device) {
-            var outgoingConnection = device.connect(params);
-            outgoingConnection.on("ringing", function () {
-                log("Ringing...");
-            });
-        }
+        $(".btnAcceptCall").bind("click", function () {
+          $(".modal").modal("hide");
+          log("Accepted call ...");
+          conn.accept();
+        });
+      });
+    })
+    .catch(function (err) {
+      console.log(err);
+      log("Could not get a token from server!");
     });
 
-    // Bind button to hangup call
+  // Bind button to make call
+  $("#btnDial").bind("click", async function () {
+    $("#modal-dial").modal("hide");
 
-    $(".btnHangUp").bind("click", function () {
-        $(".modal").modal("hide");
-        log("Hanging up...");
-        if (device) {
-            device.disconnectAll();
-        }
-    });
+    // get the phone number to connect the call to
+    var params = {
+      To: document.getElementById("phoneNumber").value,
+    };
 
-    // Activity log
-    function log(message) {
-        var logDiv = document.getElementById("log");
-        logDiv.innerHTML += "<p>&gt;&nbsp;" + message + "</p>";
-        logDiv.scrollTop = logDiv.scrollHeight;
+    // output destination number
+    $("#txtPhoneNumber").text(params.To);
+
+    console.log("Calling " + params.To + "...");
+    if (device) {
+      var outgoingConnection = device.connect(params);
+      outgoingConnection.on("ringing", function () {
+        log("Ringing...");
+      });
+
+      initiateSSE();
     }
+  });
+
+  // Bind button to hangup call
+
+  $(".btnHangUp").bind("click", function () {
+    $(".modal").modal("hide");
+    log("Hanging up...");
+    if (device) {
+      device.disconnectAll();
+    }
+  });
+
+  // SSE - Server Sent Event
+  function initiateSSE() {
+    sse = new EventSource("/getEvents?to=+9779861248030&from=+19049064208");
+    sse.addEventListener("message", function (event) {
+      console.log("message", event);
+      const data = JSON.parse(event.data);
+      log(`server-side event: ${data[data.length - 1]}`);
+    });
+
+    sse.addEventListener("closed", (event) => {
+      console.log("closed", event);
+      sse.close();
+      console.log("SSE connection Closed");
+      log(`connection to server closed`);
+    });
+
+    sse.addEventListener("error", (error) => {
+      console.log("Error", error);
+      sse.close();
+      console.log("SSE connection Closed on error");
+    });
+
+    console.log("Event source Initialized and started");
+  }
+
+  // Activity log
+  function log(message) {
+    var logDiv = document.getElementById("log");
+    logDiv.innerHTML += "<p>&gt;&nbsp;" + message + "</p>";
+    logDiv.scrollTop = logDiv.scrollHeight;
+  }
 });
